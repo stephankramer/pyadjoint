c46979fe703ef5cea16b55b19bef19daf3daf24c
Stephan Kramer <s.kramer@imperial.ac.uk>
2017-07-26 19:14:47 +0100

    Export pause_annotation()

--- fenics_adjoint/ui.py
+++ fenics_adjoint/ui.py
@@ -7,7 +7,7 @@ if backend.__name__ != "firedrake":
     from .types import Expression
 from .variational_solver import (NonlinearVariationalProblem, NonlinearVariationalSolver,
                                  LinearVariationalProblem, LinearVariationalSolver)
-from pyadjoint.tape import Tape, set_working_tape, get_working_tape
+from pyadjoint.tape import Tape, set_working_tape, get_working_tape, pause_annotation
 from pyadjoint.reduced_functional import ReducedFunctional
 from pyadjoint.verification import taylor_test, taylor_test_multiple
 from pyadjoint.drivers import compute_gradient, Hessian
